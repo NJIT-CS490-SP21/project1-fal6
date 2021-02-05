0072b874b6ad3e9311841ac73f0d5a7abb67705d
Felix Luciano Salomon <felixluciano.a@gmail.com>
2021-02-05 23:34:39 +0000

    Added functionality for retrieving a random artist from a list

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import requests
-import os
+import requests, os, random
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template
 
@@ -11,6 +10,8 @@ spotify_id = os.getenv("SPOT_ID")
 spotify_secret = os.getenv("SPOT_SECRET")
 
 auth_url="https://accounts.spotify.com/api/token" 
+artist_url="https://api.spotify.com/v1/artists/"
+
 
 auth = requests.post(auth_url,
     {
@@ -28,7 +29,24 @@ headers = {
 # main page
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    artist, name = get_artist()
+    song = get_song(artist)
+    return render_template("index.html",song = song,name = name)
+
+def get_song(artist):
+    song = "Hello there"
+    return song
+    
+def get_artist():
+    artists = ["6qqNVTkY8uBg9cP3Jd7DAH",
+    "4q3ewBCX7sLwd24euuV69X",
+    "7Ln80lUS6He07XvHI8qqHH",]
+    artist = artists[random.randint(0,2)]
+    name=requests.get(
+        artist_url+artist,
+        headers=headers,
+    ).json()["name"]
+    return artist,name
 
 app.run(
     port=int(os.getenv("PORT",8080)),
