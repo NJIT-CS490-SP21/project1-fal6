1cf39d2630cefbbce78dd5e13a6755dcfdc2512f
Felix Luciano Salomon <felixluciano.a@gmail.com>
2021-02-05 22:09:46 +0000

    Added authorization for spotify API

--- main.py
+++ main.py
@@ -5,9 +5,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+load_dotenv(find_dotenv()) # Loads API keys
+spotify_id = os.getenv("SPOT_ID")
+spotify_secret = os.getenv("SPOT_SECRET")
+
+auth_url="https://accounts.spotify.com/api/token"
+
+auth = requests.post(auth_url,
+    {
+        'grant_type' : 'client_credentials',
+        'client_id' : spotify_id,
+        'client_secret' : spotify_secret,
+    }
+)
+auth_token = auth.json()["access_token"]
+
+
+
 @app.route('/')
 def hello_world():
     return "Hello World"
+
 app.run(
     port=int(os.getenv("PORT",8080)),
     host=os.getenv("IP","0.0.0.0"),
