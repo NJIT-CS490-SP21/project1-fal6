0ddebde8f87a61c7af8972c95a824d09d41ffc62
Felix Luciano Salomon <felixluciano.a@gmail.com>
2021-02-05 22:57:30 +0000

    Added html and css files

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 load_dotenv(find_dotenv()) # Loads API keys
 spotify_id = os.getenv("SPOT_ID")
 spotify_secret = os.getenv("SPOT_SECRET")
 
-auth_url="https://accounts.spotify.com/api/token"
+auth_url="https://accounts.spotify.com/api/token" 
 
 auth = requests.post(auth_url,
     {
@@ -18,13 +19,16 @@ auth = requests.post(auth_url,
         'client_secret' : spotify_secret,
     }
 )
-auth_token = auth.json()["access_token"]
+auth_token = auth.json()["access_token"] # authorization token
 
+headers = {
+    'Authorization': f'Bearer {auth_token}'
+} # header for all requests
 
-
+# main page
 @app.route('/')
 def hello_world():
-    return "Hello World"
+    return render_template("index.html")
 
 app.run(
     port=int(os.getenv("PORT",8080)),
