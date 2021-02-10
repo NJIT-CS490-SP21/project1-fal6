f2a532e489172fd2de32dccf364cefbbcadb002d
Felix Luciano <felixluciano.a@gmail.com>
2021-02-10 00:06:14 -0500

    Fixed more song matching errors

--- genius.py
+++ genius.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv, find_dotenv
-from unidecode import unidecode
+from unidecode import unidecode #To remove tildes and such
 import requests, os
 
 load_dotenv(find_dotenv()) # Loads API keys
@@ -12,14 +12,19 @@ headers = {
 } # header for all requests
 
 def get_lyric_link(song_name,artist_name):
-    if "(with" in song_name:
+    '''
+    Returns a url for the lyrics of a given song
+    '''
+    if "(with" in song_name: #Cleans songs with colabs for easier search
         song_name = song_name[:song_name.index("(with")-1]
+
     data = {'q': song_name+' by '+artist_name}
     response = requests.get(search_url,headers=headers,data=data)
     response_json = response.json()
-    for result in response_json["response"]["hits"]:
+    
+    for result in response_json["response"]["hits"]: #For each result
         if unidecode(artist_name.lower()) in unidecode(result["result"]["primary_artist"]["name"].lower()):
             return result["result"]["url"]
-    print(response_json["response"]["hits"])
-    return None
-    
+
+    return response_json["response"]["hits"][0]["result"]["url"]
+
