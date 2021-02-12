809b28b16c4220cf9ba09b9735c6fea48620d673
Felix Luciano Salomon <felixluciano.a@gmail.com>
2021-02-12 04:09:20 +0000

    Added boilerplate user input form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from spotify import get_artist, get_song, get_songs # Handles all spotify requests
 from genius import get_lyric_link, get_artist_info
 
@@ -7,8 +7,8 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # main page
-@app.route('/')
-def hello_world():
+@app.route('/random')
+def hello_world(name):
     artist, name = get_artist()
     songs = get_songs(artist)
     song = get_song(songs)
@@ -19,6 +19,13 @@ def hello_world():
         song = song,name = name,
         lyric=lyric_link,info=info)
 
+@app.route('/', methods=['POST','GET'])
+def my_form_post():
+    if request.method == 'POST':
+        text = request.form['text']
+        print(text)
+        return redirect(url_for('hello_world',name=text))
+    return render_template("search.html")
 
 app.run(
     port=int(os.getenv("PORT",8080)),
